ff169f8d6f890fcaecb8bd49899e8ba94de15265
ngaretou <corey.garrett@sim.org>
2022-11-14 10:44:30 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/nov14_flutter_create/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nov14_flutter_create
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,13 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import url_launcher_macos
+
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
